d0f3199951cb47de37ad9fc4e9e28b7b0932c52f
BlissYang <2812420513@qq.com>
2019-08-07 11:10:30 +0800

    initialized

---
+++ vuebase/vuebase/src/components/A.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="">
+ 组件A：{{ msg }}
+    <button @click="change" type="button" name="button">change</button>
+   </div>
+</template>
+
+<script>
+export default {
+   name: '',
+   data() {
+       return {
+           msg:"默认"
+       }
+   },
+   methods:{
+    change(){
+      this.msg = '改变'
+    }
+  },
+  components: {}
+}
+</script>
+
+<style scoped >
+</style>
