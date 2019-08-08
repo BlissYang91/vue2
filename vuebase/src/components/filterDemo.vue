abb7ac2022fc3e342b93eeb039608f5dc2dc12a8
BlissYang <2812420513@qq.com>
2019-08-08 10:11:24 +0800

    过滤器

---
+++ vuebase/src/components/filterDemo.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="filter">
+        {{money | rmb}}   
+        {{text | author}}   
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'filterDemo',
+   data() {
+       return {
+           money:20,
+           text:"秦时明月"
+       }
+   },
+   filters:{
+    rmb:function(value){
+        if(!value) return;
+        value = value.toString();
+        return "$"+value;
+    },
+    author:function(value){
+        if(!value) return;
+        return value + "--来自星星的你"
+    }
+   },
+  components: {}
+}
+</script>
+
+<style scoped >
+</style>
